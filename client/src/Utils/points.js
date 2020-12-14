7ccc5ed3c960196f1f27037fdc94d93f8307f516
Tomas <tomaspasqualini@gmail.com>
2020-12-14 11:48:45 -0300

    nada

--- client/src/Utils/points.js
+++ client/src/Utils/points.js
@@ -20,30 +20,24 @@ const pointsSchema = [
     question: '¿Cuál es tu experiencia con la copa menstrual?',
     answers: {
       'Nunca usé una copa menstrual.': {
-        points: ['rectaS', 'curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL','rectaL'],
         exclude: []
-        // saque rectaL
       },
       'La copa me parece larga para mí, o tuve que cortarle el tallo.': {
         points: ['curvaS', 'curvaL'],
         exclude: []
       },
       'La copa me es difícil de agarrar o extraer.': {
-        points: ['rectaS', 'curvaL'],
-        exclude: ['curvaS']
-        //se elimino  'curvaL' del exclude
-        //se agrego CurvaL
-        //se sacó RectaL
+        points: ['rectaS', 'rectaL'],
+        exclude: ['curvaS','curvaL']
       },
       'Me cuesta que la copa se abra totalmente.': { 
-        points: ['rectaS', 'rectaL', 'curvaL'], 
+        points: ['rectaS', 'rectaL', 'curvaL','curvaS'], 
         exclude: [] 
-        //se agrego CurvaL
       },
       'Mi copa menstrual me resulta Ok, pero me gustaría probar una diferente.': {
-        points: ['rectaS', 'curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL','rectaL'],
         exclude: []
-        //se elimino  'rectaL' de puntos
       }
     }
   },
@@ -51,13 +45,12 @@ const pointsSchema = [
     question: 'Prefiero tener una copa menstrual que sea, por sobre todo...',
     answers: {
       'Más fácil de colocar y extraer de mi cuerpo': { 
-        points: ['rectaS','curvaL'], 
+        points: ['rectaS','rectaL'], 
         exclude: [] },
-        //se agregó 'curvaL'
-        //Se elimino la rectaL
       "Extra comodidad, pero que requiere un poco más de 'autoconocimiento' y práctica al colocar y extraer": {
-        points: ['curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL'],
         exclude: []
+        //agregue rectaS
       }
     }
   },
@@ -65,9 +58,8 @@ const pointsSchema = [
     question: '¿Cuál es la altura de tu cérvix?',
     answers: {
       'Tengo cérvix alto. No llego o apenas llego a tocarlo con mi dedo más largo (más de 55mm)': {
-        points: ['rectaS', 'rectaL','curvaL'],
+        points: ['rectaS', 'rectaL'],
         exclude: []
-        //se agrego 'curvaL'
       },
       'Tengo cérvix medio. Llego a tocarlo con dos falanges aproximadamente (entre 45mm y 55mm)': {
         points: ['rectaS', 'rectaL', 'curvaS', 'curvaL'],
@@ -78,9 +70,8 @@ const pointsSchema = [
         exclude: []
       },
       'No tengo idea. Prefiero no verificarlo.': {
-        points: ['rectaS', 'curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL','rectaL'],
         exclude: []
-        //sacamos RectaL
       }
     }
   },
@@ -88,18 +79,17 @@ const pointsSchema = [
     question: '¿Cómo te llevás con el deporte?',
     answers: {
       'No me gusta mucho el deporte.': {
-        points: ['rectaS', 'curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL','rectaL'],
         exclude: []
-        //Sacamos RectaL
       },
       'Trato de hacer deporte 1 vez por semana.': {
-        points: ['rectaS', 'curvaS', 'curvaL'],
+        points: ['rectaS', 'curvaS', 'curvaL', 'rectaL'],
         exclude: []
-        //se elimino rectaL
       },
       'Soy deportista, o hago actividad física más de 2 veces por semana.': {
-        points: ['curvaS', 'curvaL'],
+        points: ['curvaS', 'curvaL','rectaS', 'rectaL'],
         exclude: []
+        //agrego recta. 
       }
     }
   }
