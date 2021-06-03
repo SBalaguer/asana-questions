4340c2a69de91642be854c35b6c0dee30215b272
Tomas <tomaspasqualini@gmail.com>
2021-06-03 12:27:16 -0300

    cambio de puntajes, mucho mini

--- client/src/Utils/points.js
+++ client/src/Utils/points.js
@@ -4,18 +4,18 @@ const pointsSchema = [
     answers: {
       'Menos de 15 años': { 
         points: ['mini'], 
-        exclude: ['rectaL', 'curvaL','rectaS', 'curvaS'] },
+        exclude: [] },
       '15 a 18 años': { 
-        points: [], 
+        points: ['mini'], 
         exclude: [] },
       '19 a 24 años': { 
-        points: [], 
+        points: ['rectaL', 'curvaL','rectaS', 'curvaS'], 
         exclude: [] },
       '25 a 35 años': { 
-        points: [], 
+        points: ['rectaL', 'curvaL','rectaS', 'curvaS'], 
         exclude: [] },
       '36 años o más': { 
-        points: [], 
+        points: ['rectaL', 'curvaL','rectaS', 'curvaS'], 
         exclude: [] }
     }
   },
@@ -23,28 +23,28 @@ const pointsSchema = [
     question: '¿Tuviste partos?',
     answers: {
       'No tuve partos': { 
-        points: ['rectaS', 'curvaS'], 
+        points: ['rectaS','curvaS','mini'], 
         exclude: ['rectaL', 'curvaL'] },
       'Sí, tuve parto natural': { 
         points: ['rectaL', 'curvaL'], 
-        exclude: ['rectaS', 'curvaS'] },
+        exclude: ['rectaS', 'curvaS','mini'] },
       'Sí, tuve parto por cesárea': { 
         points: ['rectaS', 'curvaS'], 
         exclude: ['rectaL', 'curvaL'] },
       'Sí, tuve parto por cesárea y natural': { 
         points: ['rectaL', 'curvaL'], 
-        exclude: ['rectaS', 'curvaS'] }
+        exclude: ['rectaS', 'curvaS','mini'] }
     }
   },
   {
     question: '¿Cuál es tu experiencia con la copa menstrual?',
     answers: {
       'Nunca usé una copa menstrual.': {
-        points: ['rectaS', 'curvaS','rectaL','curvaL'],
+        points: ['rectaS', 'curvaS','rectaL','curvaL','mini'],
         exclude: []
       },
       'La copa me parece larga para mí, o tuve que cortarle el tallo.': {
-        points: ['curvaS', 'curvaL'],
+        points: ['curvaS', 'curvaL','mini'],
         exclude: []
       },
       'La copa me es difícil de agarrar o extraer.': {
@@ -52,11 +52,11 @@ const pointsSchema = [
         exclude: []
       },
       'La copa me pierde o cuesta que se abra totalmente.': { 
-        points: ['rectaS', 'rectaL','curvaS','curvaL'],
+        points: ['curvaS','curvaL','mini'],
         exclude: [] 
       },
       'Mi copa menstrual está bien, pero me gustaría probar una diferente.': {
-        points: ['curvaS','curvaL','rectaS', 'rectaL'],
+        points: ['curvaS','curvaL','mini'],
         exclude: []
       }
     }
@@ -65,10 +65,10 @@ const pointsSchema = [
     question: 'Prefiero tener una copa menstrual que sea, por sobre todo...',
     answers: {
       'Más fácil de colocar y extraer de mi cuerpo': { 
-        points: ['rectaS','rectaL','curvaS','curvaL'], 
+        points: ['rectaS','rectaL'], 
         exclude: [] },
       "Extra comodidad, aunque requiera un poco más de práctica al colocar y extraer.": {
-        points: ['curvaS','curvaL'],
+        points: ['curvaS','curvaL','mini'],
         exclude: []
       }
     }
@@ -86,10 +86,10 @@ const pointsSchema = [
       },
       'Tengo cérvix bajo. Lo toco con una falange de dedo o menos (44mm o menos)': {
         points: ['mini'],
-        exclude: ['curvaS','curvaL','rectaS', 'rectaL']
+        exclude: []
       },
       'No tengo idea. Prefiero no verificarlo.': {
-        points: ['rectaS', 'curvaS', 'rectaL','curvaL'],
+        points: ['rectaS', 'curvaS', 'rectaL','curvaL','mini'],
         exclude: []
       }
     }
@@ -98,15 +98,15 @@ const pointsSchema = [
     question: '¿Cómo te llevás con el deporte?',
     answers: {
       'No me gusta mucho el deporte.': {
-        points: ['rectaS', 'curvaS', 'rectaL','curvaL'],
+        points: ['rectaS', 'curvaS', 'rectaL','curvaL','mini'],
         exclude: []
       },
       'Trato de hacer deporte 1 vez por semana.': {
-        points: ['curvaS', 'curvaL'],
+        points: ['curvaS', 'curvaL','mini'],
         exclude: []
       },
       'Soy deportista, o hago actividad física más de 2 veces por semana.': {
-        points: ['curvaS', 'curvaL'],
+        points: ['curvaS', 'curvaL','mini'],
         exclude: ['rectaS','rectaL',]
       }
     }
