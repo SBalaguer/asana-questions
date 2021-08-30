a41a8d444dae6c8262e828a9c725b61c21bbd66e
Santiago Balaguer <santi@Santiagos-MacBook-Pro.local>
2021-08-30 20:05:58 +0200

    agregar telefono a base shopify

--- routes/customers.js
+++ routes/customers.js
@@ -8,40 +8,43 @@ const axios = require('axios');
 router.get('/getCustomer', async (req, res, next) => {
     const email = req.query.email;
     const phone = req.query.phone;
+    console.log(phone);
     const name = req.query.name;
     const reco = req.query.reco;
     const answers = JSON.parse(req.query.answers);
-    const tags = answers.reduce((acc,val,index)=> acc+=`${val.tag},`,"")+reco;
+    const tags = answers.reduce((acc, val, index) => acc += `${val.tag},`, "") + reco;
     let id;
     try {
         const user = await axios.get(`https://${process.env.API_CUSTOMER_KEY}:${process.env.API_CUSTOMER_SECRET}@asana-cup.myshopify.com/admin/api/2021-04/customers/search.json?query=email:${email}&fields=email,id`);
         if (user.data.customers.length) {
             id = user.data.customers[0].id;
-            const response = await axios.put(`https://${process.env.API_CUSTOMER_KEY}:${process.env.API_CUSTOMER_SECRET}@asana-cup.myshopify.com/admin/api/2021-04/customers/${id}.json`,{
+            const response = await axios.put(`https://${process.env.API_CUSTOMER_KEY}:${process.env.API_CUSTOMER_SECRET}@asana-cup.myshopify.com/admin/api/2021-04/customers/${id}.json`, {
                 "customer": {
                     "id": id,
                     "tags": tags,
                     "accepts_marketing": true,
-                    "marketing_opt_in_level": "single_opt_in"
+                    "marketing_opt_in_level": "single_opt_in",
+                    ...(!phone && { "phone": phone })
                 }
             });
-            res.json({success:true, type: "update" ,user:user.data.customers });
+            res.json({ success: true, type: "update", user: user.data.customers });
         } else {
-            const response = await axios.post(`https://${process.env.API_CUSTOMER_KEY}:${process.env.API_CUSTOMER_SECRET}@asana-cup.myshopify.com/admin/api/2021-04/customers.json`,{
+            const response = await axios.post(`https://${process.env.API_CUSTOMER_KEY}:${process.env.API_CUSTOMER_SECRET}@asana-cup.myshopify.com/admin/api/2021-04/customers.json`, {
                 "customer": {
                     "first_name": name,
                     "email": email,
-                    "answers":answers,
-                    "tags":tags,
+                    "answers": answers,
+                    "tags": tags,
                     "accepts_marketing": true,
-                    "marketing_opt_in_level": "single_opt_in"
-                  }                 
+                    "marketing_opt_in_level": "single_opt_in",
+                    ...(!phone && { "phone": phone })
+                }
             });
-            res.json({success:true, type: "update" ,user:user.data.customers });
+            res.json({ success: true, type: "update", user: user.data.customers });
         }
     } catch (error) {
         res.json({ success: false, error: { message: error.message } }).status(500);
     }
-  });
+});
 
 module.exports = router;
