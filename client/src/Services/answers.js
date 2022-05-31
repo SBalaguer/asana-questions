b8f7e0042f7401a3fb0825c88d4d0c6782db1bfa
Santi Balaguer <santibalaguer@Santis-MBP.Home>
2022-05-31 08:50:34 +0100

    hot fix tag generator

--- client/src/Services/answers.js
+++ client/src/Services/answers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  // baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5001",
   withCredentials: true
 });
 
