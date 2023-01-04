11560cc09ea54762415dc6e37f17da6b6e926943
Santi Balaguer <santibalaguer@Santis-MBP.fibertel.com.ar>
2023-01-04 23:14:49 +0100

    specified header address to CORS

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.FRONT_END_URL);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
